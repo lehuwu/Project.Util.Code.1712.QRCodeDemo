87b5e327a36a5839340cff0fc76f9f066c4aba72
aserfend <liuchangm7j@vip.qq.com>
2018-01-28 20:48:08 +0800

    init

---
+++ 联系人名片/FormMain.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace 联系人名片
+{
+	partial class FromMain
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.PalMain = new System.Windows.Forms.Panel();
+			this.BtnInput = new System.Windows.Forms.Button();
+			this.BtnOutPut = new System.Windows.Forms.Button();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.TextBoxUserIcon = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.TextBoxOutputPath = new System.Windows.Forms.TextBox();
+			this.TextBoxInputPath = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.PicBoxShowContactImg = new System.Windows.Forms.PictureBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.TextBoxShowTmpInfo = new System.Windows.Forms.TextBox();
+			this.LstBoxShowUserInfo = new System.Windows.Forms.ListBox();
+			this.PalMain.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.PicBoxShowContactImg)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// PalMain
+			// 
+			this.PalMain.Controls.Add(this.BtnInput);
+			this.PalMain.Controls.Add(this.BtnOutPut);
+			this.PalMain.Controls.Add(this.groupBox3);
+			this.PalMain.Controls.Add(this.groupBox2);
+			this.PalMain.Controls.Add(this.groupBox1);
+			this.PalMain.Controls.Add(this.LstBoxShowUserInfo);
+			this.PalMain.Location = new System.Drawing.Point(12, 12);
+			this.PalMain.Name = "PalMain";
+			this.PalMain.Size = new System.Drawing.Size(986, 570);
+			this.PalMain.TabIndex = 2;
+			// 
+			// BtnInput
+			// 
+			this.BtnInput.Location = new System.Drawing.Point(796, 524);
+			this.BtnInput.Name = "BtnInput";
+			this.BtnInput.Size = new System.Drawing.Size(87, 35);
+			this.BtnInput.TabIndex = 9;
+			this.BtnInput.Text = "导入";
+			this.BtnInput.UseVisualStyleBackColor = true;
+			this.BtnInput.Click += new System.EventHandler(this.BtnInput_Click);
+			// 
+			// BtnOutPut
+			// 
+			this.BtnOutPut.Location = new System.Drawing.Point(889, 524);
+			this.BtnOutPut.Name = "BtnOutPut";
+			this.BtnOutPut.Size = new System.Drawing.Size(87, 35);
+			this.BtnOutPut.TabIndex = 8;
+			this.BtnOutPut.Text = "导出";
+			this.BtnOutPut.UseVisualStyleBackColor = true;
+			this.BtnOutPut.Click += new System.EventHandler(this.BtnOutPut_Click);
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.TextBoxUserIcon);
+			this.groupBox3.Controls.Add(this.label3);
+			this.groupBox3.Controls.Add(this.TextBoxOutputPath);
+			this.groupBox3.Controls.Add(this.TextBoxInputPath);
+			this.groupBox3.Controls.Add(this.label2);
+			this.groupBox3.Controls.Add(this.label1);
+			this.groupBox3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.groupBox3.Location = new System.Drawing.Point(595, 339);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(381, 145);
+			this.groupBox3.TabIndex = 7;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "设置";
+			// 
+			// TextBoxUserIcon
+			// 
+			this.TextBoxUserIcon.Location = new System.Drawing.Point(58, 102);
+			this.TextBoxUserIcon.Name = "TextBoxUserIcon";
+			this.TextBoxUserIcon.Size = new System.Drawing.Size(313, 34);
+			this.TextBoxUserIcon.TabIndex = 4;
+			this.TextBoxUserIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.SelectFileName);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label3.Location = new System.Drawing.Point(6, 105);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(52, 27);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "图标";
+			// 
+			// TextBoxOutputPath
+			// 
+			this.TextBoxOutputPath.Location = new System.Drawing.Point(58, 65);
+			this.TextBoxOutputPath.Name = "TextBoxOutputPath";
+			this.TextBoxOutputPath.Size = new System.Drawing.Size(313, 34);
+			this.TextBoxOutputPath.TabIndex = 3;
+			this.TextBoxOutputPath.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.SelectFilePath);
+			// 
+			// TextBoxInputPath
+			// 
+			this.TextBoxInputPath.Location = new System.Drawing.Point(58, 28);
+			this.TextBoxInputPath.Name = "TextBoxInputPath";
+			this.TextBoxInputPath.Size = new System.Drawing.Size(313, 34);
+			this.TextBoxInputPath.TabIndex = 2;
+			this.TextBoxInputPath.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.SelectFileName);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label2.Location = new System.Drawing.Point(6, 68);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(52, 27);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "输出";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label1.Location = new System.Drawing.Point(6, 31);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(52, 27);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "输入";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.PicBoxShowContactImg);
+			this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.groupBox2.Location = new System.Drawing.Point(225, 3);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(751, 330);
+			this.groupBox2.TabIndex = 6;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "预览";
+			// 
+			// PicBoxShowContactImg
+			// 
+			this.PicBoxShowContactImg.BackColor = System.Drawing.Color.White;
+			this.PicBoxShowContactImg.Location = new System.Drawing.Point(6, 24);
+			this.PicBoxShowContactImg.Name = "PicBoxShowContactImg";
+			this.PicBoxShowContactImg.Size = new System.Drawing.Size(735, 300);
+			this.PicBoxShowContactImg.TabIndex = 7;
+			this.PicBoxShowContactImg.TabStop = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.TextBoxShowTmpInfo);
+			this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.groupBox1.Location = new System.Drawing.Point(225, 339);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(364, 220);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "内容";
+			// 
+			// TextBoxShowTmpInfo
+			// 
+			this.TextBoxShowTmpInfo.Location = new System.Drawing.Point(6, 30);
+			this.TextBoxShowTmpInfo.Multiline = true;
+			this.TextBoxShowTmpInfo.Name = "TextBoxShowTmpInfo";
+			this.TextBoxShowTmpInfo.Size = new System.Drawing.Size(352, 182);
+			this.TextBoxShowTmpInfo.TabIndex = 0;
+			this.TextBoxShowTmpInfo.TextChanged += new System.EventHandler(this.TextBoxShowTmpInfo_TextChanged);
+			// 
+			// LstBoxShowUserInfo
+			// 
+			this.LstBoxShowUserInfo.FormattingEnabled = true;
+			this.LstBoxShowUserInfo.ItemHeight = 15;
+			this.LstBoxShowUserInfo.Location = new System.Drawing.Point(0, 0);
+			this.LstBoxShowUserInfo.Name = "LstBoxShowUserInfo";
+			this.LstBoxShowUserInfo.Size = new System.Drawing.Size(219, 559);
+			this.LstBoxShowUserInfo.TabIndex = 2;
+			this.LstBoxShowUserInfo.SelectedIndexChanged += new System.EventHandler(this.LstBoxShowUserInfo_SelectedIndexChanged);
+			// 
+			// FromMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1014, 589);
+			this.Controls.Add(this.PalMain);
+			this.Name = "FromMain";
+			this.Text = "联系人名片";
+			this.Load += new System.EventHandler(this.FromMain_Load);
+			this.PalMain.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.PicBoxShowContactImg)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel PalMain;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.PictureBox PicBoxShowContactImg;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ListBox LstBoxShowUserInfo;
+		private System.Windows.Forms.TextBox TextBoxOutputPath;
+		private System.Windows.Forms.TextBox TextBoxInputPath;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox TextBoxUserIcon;
+		private System.Windows.Forms.Button BtnInput;
+		private System.Windows.Forms.Button BtnOutPut;
+		private System.Windows.Forms.TextBox TextBoxShowTmpInfo;
+	}
+}
+
---
+++ 联系人名片/FormMain.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNet4.Utilities.UtilReg;
+using DotNet4.Utilities.UtilCode;
+using System.IO;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace 联系人名片
+{
+	public partial class FromMain : Form
+	{
+		public FromMain()
+		{
+			InitializeComponent();
+			TextBoxUserIcon.HandleCreated += LoadControlData;
+			TextBoxUserIcon.HandleDestroyed += SaveControlData;
+			TextBoxInputPath.HandleCreated += LoadControlData;
+			TextBoxInputPath.HandleDestroyed += SaveControlData;
+			TextBoxOutputPath.HandleCreated += LoadControlData;
+			TextBoxOutputPath.HandleDestroyed += SaveControlData;
+		}
+
+		private void FromMain_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void SelectFilePath(object sender, EventArgs e)
+		{
+			if (sender is TextBox tar)
+			{
+				FolderBrowserDialog fileDialog = new FolderBrowserDialog();
+				Reg a = new Reg();
+				Reg b = a.In("userInfo");
+				if (b.GetInfo<string>("User") == "")
+					fileDialog.Description = "技术支持请联系qq:331945833";
+				else
+					fileDialog.Description = "文件所在路径";
+				fileDialog.ShowNewFolderButton = true;
+				fileDialog.SelectedPath = tar.Text.Length == 0 ? Application.StartupPath : tar.Text;
+				if (fileDialog.ShowDialog() == DialogResult.OK)
+				{
+					string file = fileDialog.SelectedPath;//返回文件的完整路径   
+					tar.Text = file;
+				}
+			}
+		}
+		private void SelectFileName(object sender, EventArgs e)
+		{
+			OpenFileDialog fileDialog = new OpenFileDialog();
+			if (sender is TextBox tar)
+			{
+				if (tar.Name != "TextBoxInputPath")
+					fileDialog.Filter = "(图片)|*.png;*.jpg;*.bmp;*.ico;*.jpge;*.gif;*.psd;*.psb|(全部文件)|*.*";
+				else
+					fileDialog.Filter = "(文本)|*.txt|(全部文件)|*.*";
+				fileDialog.RestoreDirectory = true;
+				if (fileDialog.ShowDialog() == DialogResult.OK) tar.Text = fileDialog.FileName;//返回文件的完整路径  
+			}
+ 
+		}
+		private void LoadControlData(object sender,EventArgs e)
+		{
+			Reg DataBinding = new Reg().In("Setting").In("FormData");
+			if(sender is Control tar)
+			{
+				tar.Text = DataBinding.GetInfo<string>(tar.Name);
+			}
+		}
+		private void SaveControlData(object sender, EventArgs e)
+		{
+			Reg DataBinding = new Reg().In("Setting").In("FormData");
+			if (sender is Control tar)
+			{
+				DataBinding.SetInfo(tar.Name,tar.Text);
+			}
+		}
+		private string[] fileInfo;
+		private void LoadDataList()
+		{
+			LstBoxShowUserInfo.Items.Clear();
+			fileInfo = GetFileData();
+			foreach (string tmp in fileInfo)
+			{
+				LstBoxShowUserInfo.Items.Add(HttpUtil.GetElement(tmp,"FN:","\r"));
+			}
+		}
+		private string[] GetFileData()
+		{
+			using (StreamReader s = new StreamReader(TextBoxInputPath.Text, Encoding.Default))
+			{
+				return s.ReadToEnd().Split(new string[] { "BEGIN:VCARD" },StringSplitOptions.RemoveEmptyEntries);
+			}
+		}
+		private void BtnInput_Click(object sender, EventArgs e)
+		{
+			LoadDataList();
+		}
+
+		private void BtnOutPut_Click(object sender, EventArgs e)
+		{
+			for(int i = 0; i < LstBoxShowUserInfo.Items.Count; i++)
+			{
+				LstBoxShowUserInfo.SelectedIndex = i;
+				OutPutImg("[" + i +"]" + LstBoxShowUserInfo.SelectedItem.ToString()+".png");
+			}
+			
+		}
+
+		private void OutPutImg(string fileName)
+		{
+			using(Graphics g = Graphics.FromImage(PicBoxShowContactImg.Image))
+			{
+				PicBoxShowContactImg.Image.Save(TextBoxOutputPath.Text + "\\" + fileName, ImageFormat.Png);
+			}
+		}
+		private void LstBoxShowUserInfo_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if(sender is ListBox tar)
+			{
+				if (tar.SelectedIndex == -1) return;
+				string info = "BEGIN:VCARD" + fileInfo[tar.SelectedIndex];
+				TextBoxShowTmpInfo.Text = info;
+			}
+		}
+		private Image GetContactCard(Image QRCode,string info)
+		{
+			BufferedGraphicsContext current = BufferedGraphicsManager.Current; //(1)
+			PicBoxShowContactImg.Image = new Bitmap(PicBoxShowContactImg.DisplayRectangle.Width,PicBoxShowContactImg.DisplayRectangle.Height );
+			using (BufferedGraphics bg = current.Allocate(Graphics.FromImage(PicBoxShowContactImg.Image), PicBoxShowContactImg.DisplayRectangle))
+			{  //(2)
+				using (Graphics g = bg.Graphics)
+				{
+					g.Clear(Color.White);
+					int size = Convert.ToInt32(PicBoxShowContactImg.Width * 0.01);
+					using (LinearGradientBrush BrushOuter = new LinearGradientBrush(new PointF(0, 0), new PointF(0, size * 100), Color.FromArgb(255, 100, 100, 200), Color.FromArgb(50, 100, 100, 255)))
+					{
+						g.FillRectangle(BrushOuter, 0, 0, size * 100, size * 100);
+					}
+					g.DrawImage(QRCode, new Rectangle(size * 55, size * 5, size * 30, size * 30));
+					var user = new
+					{
+						name = HttpUtil.GetElement(info, "FN:", "\r"),
+						tel = HttpUtil.GetElement(info, "TEL:", "\r"),
+						email = HttpUtil.GetElement(info, "EMAIL:", "\r"),
+						address = HttpUtil.GetElement(info, "ADR:", "\r"),
+						org = HttpUtil.GetElement(info, "ORG:", "\r"),
+					};
+					using (SolidBrush brh = new SolidBrush(Color.FromArgb(255, 0, 0, 0)))
+					{
+						using (Font f = new Font("微软雅黑", 16F, FontStyle.Regular))
+						{
+							PointF p = new PointF(size * 5, size * 5);
+
+							GAddNewInfo("姓名", user.name, ref p, brh, f, g);
+							GAddNewInfo("电话", user.tel, ref p, brh, f, g);
+							GAddNewInfo("邮箱", user.email, ref p, brh, f, g);
+							GAddNewInfo("地址", user.address, ref p, brh, f, g);
+							GAddNewInfo("组织", user.org, ref p, brh, f, g);
+						}
+
+					}
+					bg.Render();
+				}
+			}
+			return QRCode;
+		}
+		private void GAddNewInfo(string tag,string info, ref PointF point,SolidBrush b,Font font,Graphics g)
+		{
+			if (info == null || info.Length == 0) return;
+			g.DrawString(tag + "   " + info, font, b, point);
+			point.Y +=(float)( font.Height*1.1);
+		}
+		/// <summary>
+		/// 生成二维码，如果有Logo，则在二维码中添加Logo
+		/// </summary>
+		/// <param name="content"></param>
+		/// <returns></returns>
+		private Bitmap CreateQRCode(string content)
+		{
+			QRCodeEncoder qrEncoder = new QRCodeEncoder
+			{
+				QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE,
+				QRCodeScale = 4,
+				QRCodeVersion = 9,
+				QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M
+			};
+			try
+			{
+				Bitmap qrcode = qrEncoder.Encode(content, Encoding.UTF8);
+				if (!TextBoxUserIcon.Text.Equals(string.Empty))
+				{
+					Graphics g = Graphics.FromImage(qrcode);
+					Bitmap bitmapLogo = new Bitmap(TextBoxUserIcon.Text);
+					int logoSize = Convert.ToInt32(30);
+					bitmapLogo = new Bitmap(bitmapLogo, new System.Drawing.Size(logoSize, logoSize));
+					PointF point = new PointF(qrcode.Width / 2 - logoSize / 2, qrcode.Height / 2 - logoSize / 2);
+					g.DrawImage(bitmapLogo, point);
+				}
+				return qrcode;
+			}
+			catch (IndexOutOfRangeException ex)
+			{
+				MessageBox.Show("超出当前二维码版本的容量上限，请选择更高的二维码版本！", "系统提示");
+				return new Bitmap(100, 100);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("生成二维码出错！"+ ex.Message , "系统提示");
+				return new Bitmap(100, 100);
+			}
+		}
+
+		private void TextBoxShowTmpInfo_TextChanged(object sender, EventArgs e)
+		{
+			Image bimg = CreateQRCode(TextBoxShowTmpInfo.Text);
+			GetContactCard(bimg, TextBoxShowTmpInfo.Text);
+		}
+	}
+}
